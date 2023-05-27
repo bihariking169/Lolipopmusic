a626325f17dd1a198b63ba3b28a8dbfea4f1e0c2
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-05-27 21:09:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/WCGKING/BRANDED-MUSIC",
+    "https://github.com/WCGKING/BRANDED-MASTI",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
