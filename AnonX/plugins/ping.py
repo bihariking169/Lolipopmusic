e6d6839d0da11277a2eb633726acc8756f964338
KrishnaxMusic <146642087+KrishnaxMusic@users.noreply.github.com>
2023-10-02 22:05:34 +0530

    Update ping.py

--- AnonX/plugins/ping.py
+++ AnonX/plugins/ping.py
@@ -25,12 +25,73 @@ async def ping_com(client, message: Message, _):
         caption=_["ping_1"],
     )
     start = datetime.now()
-    pytgping = await Anon.ping()
+    pytgping = await Vip.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
+        _["ping_3"])
+    await response.edit_text(
+        _["ping_4"])
+    await response.edit_text(
         _["ping_2"].format(
             resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+       _["ping_5"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_6"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_7"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_8"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_9"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_10"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_11"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
+        ))
+    await response.edit_text(
+        _["ping_12"].format(
+            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
         ),
-        reply_markup=close_keyboard
-    )
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+            InlineKeyboardButton(
+                text="★ ✚ ᴀᴅᴅ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ✚ ★",
+                url=f"https://t.me/{app.username}?startgroup=true",
+            )
+        
+        ],
+        [
+            InlineKeyboardButton(
+                text="★ ʙʀᴀɴᴅᴇᴅ ꜱᴜᴘᴘᴏʀᴛ ★", url=f"https://t.me/BRANDED_WORLD",
+            ),
+            InlineKeyboardButton(
+                text="★ ʙʀᴀɴᴅᴇᴅ ᴄʜᴀɴɴᴇʟ ★", url=f"https://t.me/BRANDRD_BOT",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="★  ʜᴇʟᴘ ★ ", callback_data="settings_back_helper"
+            )
+        ],
+    ]
+    ),
+)
+    
+        
