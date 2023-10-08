b3e0897b7a1a2b72dc2ebec1749b8e82d04e3cc5
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2023-10-08 14:13:42 +0530

    Update song.py

--- AnonX/utils/inline/song.py
+++ AnonX/utils/inline/song.py
@@ -15,7 +15,7 @@ def song_markup(_, vidid):
         ],
         [
             InlineKeyboardButton(
-                text="🥀 sᴜᴩᴩᴏʀᴛ 🥀", url=f"{config.SUPPORT_GROUP}",
+                text="🥀 𝐋𝐈𝐅𝐄𝐋𝐈𝐍𝐄 🥀", url=f"{config.SUPPORT_GROUP}",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
